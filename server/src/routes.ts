1eee299e7faa1f37158a396ea6a4b01835a03a36
tabaesso <tatabaesso@gmail.com>
2020-08-05 00:39:22 -0300

    criando endpoint de adição para aula+usuário+agenda

---
+++ server/src/routes.ts
@@ -0,0 +1,67 @@
+import express from 'express';
+import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
+
+const routes = express.Router();
+
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
+
+routes.post('/classes', async (req, res) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = req.body;
+
+    const trx = await db.transaction();
+
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesId = await trx('classes').insert({
+            subject,
+            cost,
+            user_id
+        });
+    
+        const class_id = insertedClassesId[0];
+    
+        const classSchedule = schedule.map((scheduleItem:ScheduleItem) => {
+            return {
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+                class_id
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        await trx.commit();
+    
+        return res.status(201).send();
+    } catch (err){
+        await trx.rollback();
+
+        return res.status(400).json({
+            error: 'Unexpected error while creating new class'
+        })
+    }
+});
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    return res.json({message: 'Hello world'});
-});
+app.use(routes);
 
 app.listen(3333);
